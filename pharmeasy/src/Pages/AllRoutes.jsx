bff4c86c92fce3fd76239cdd5a23d5395358af21
Aditi Sharma <sharmaaditi9112001@gmail.com>
2023-04-03 01:02:59 +0530

    searcg

--- pharmeasy/src/Pages/AllRoutes.jsx
+++ pharmeasy/src/Pages/AllRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Search from "./Search";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import { Products } from "../Pages/ProductPage/Products";
@@ -13,7 +12,6 @@ function AllRoutes() {
       <Route path="*" element={<NotFound />} />
       <Route path="/products" element={<Products />} />
       <Route path="/products/:id" element={<SingleProductPage />} />
-      <Route path="/search/:name" element={<Search />}></Route>
       <Route path="/cart" element={<Cart />} />
     </Routes>
   );
